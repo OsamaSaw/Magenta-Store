1a3b9745b6a72f4957ebbdab5dd23b84b7f2701b
bahaa <bahaa.tuffaha@yahoo.com>
2023-10-09 19:14:56 +0300

    adding Pagination

--- components/products-content/list/index.tsx
+++ components/products-content/list/index.tsx
@@ -7,11 +7,44 @@ import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./../../../firebase";
+import { Pagination } from "@mui/material";
+
+function paginateArray(
+  array: any[],
+  currentPage: number,
+  itemsPerPage: number
+) {
+  // Calculate the start and end indices for the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  // Use slice to extract the items for the current page
+  const pageItems = array.slice(startIndex, endIndex);
+
+  return pageItems;
+}
+
+function calculateTotalPages(array: [], itemsPerPage: number) {
+  if (itemsPerPage <= 0) {
+    return 0; // Invalid itemsPerPage
+  }
+
+  const totalItems = array.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  return totalPages;
+}
 
 const ProductsContent = () => {
   // const fetcher = (url: string) => fetch(url).then((res) => res.json());
   // const { data, error } = useSwr("/api/products", fetcher);
   const [products, setProducts] = useState<ProductType[]>([]);
+  const [page, setPage] = useState(1);
+  const [numberOfPages, setNumberOfPages] = useState(10);
+  const itemsPerPage = 9;
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
   const fetchProducts = async () => {
     await getDocs(collection(db, "ProgramDummyData")).then((querySnapshot) => {
@@ -26,6 +59,7 @@ const ProductsContent = () => {
 
   useEffect(() => {
     fetchProducts();
+    setNumberOfPages(calculateTotalPages(products, itemsPerPage));
   }, []);
 
   // if (error) return <div>Failed to load users</div>;
@@ -36,19 +70,24 @@ const ProductsContent = () => {
     <>
       {!products && <ProductsLoading />}
 
-      {products && (
+      {Boolean(products) && (
         <section className="products-list">
-          {products.map((item: ProductType) => (
-            <ProductItem
-              id={item.id}
-              name={item.name}
-              price={item.price}
-              key={item.id}
-              image={item.image}
-            />
-          ))}
+          {paginateArray(products, page, itemsPerPage).map(
+            (item: ProductType) => (
+              <ProductItem
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                key={item.id}
+                image={item.image}
+              />
+            )
+          )}
         </section>
       )}
+      <div className="w-full flex flex-row justify-center">
+        <Pagination count={numberOfPages} page={page} onChange={handleChange} />
+      </div>
     </>
   );
 };
